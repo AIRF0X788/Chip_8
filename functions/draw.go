65c09aa2e5595cf19b6559def9d157b4694f4280
Kevskoo <kevin.parvinfr@gmail.com>
2023-10-05 20:41:15 +0200

    Draw and Input Functions

---
+++ functions/draw.go
@@ -0,0 +1,22 @@
+package functions
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func (g *Chip8) Draw(screen *ebiten.Image) {
+	for row := 0; row < gfxHeight; row++ {
+		for col := 0; col < gfxWidth; col++ {
+			isOn := g.GFX[(row/gfxMultiplier)*chscreenWidth+(col/gfxMultiplier)] == 1
+			var colorToUse color.Color
+			if isOn {
+				colorToUse = color.White
+			} else {
+				colorToUse = color.Black
+			}
+			screen.Set(col, row, colorToUse)
+		}
+	}
+}
---
+++ functions/input.go
@@ -0,0 +1,24 @@
+package functions
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+func (c *Chip8) input() {
+	c.Key = [16]bool{
+		ebiten.IsKeyPressed(ebiten.KeyX),
+		ebiten.IsKeyPressed(ebiten.Key1),
+		ebiten.IsKeyPressed(ebiten.Key2),
+		ebiten.IsKeyPressed(ebiten.Key3),
+		ebiten.IsKeyPressed(ebiten.KeyQ),
+		ebiten.IsKeyPressed(ebiten.KeyW),
+		ebiten.IsKeyPressed(ebiten.KeyE),
+		ebiten.IsKeyPressed(ebiten.KeyA),
+		ebiten.IsKeyPressed(ebiten.KeyS),
+		ebiten.IsKeyPressed(ebiten.KeyD),
+		ebiten.IsKeyPressed(ebiten.KeyZ),
+		ebiten.IsKeyPressed(ebiten.KeyC),
+		ebiten.IsKeyPressed(ebiten.Key4),
+		ebiten.IsKeyPressed(ebiten.KeyR),
+		ebiten.IsKeyPressed(ebiten.KeyF),
+		ebiten.IsKeyPressed(ebiten.KeyV),
+	}
+}
